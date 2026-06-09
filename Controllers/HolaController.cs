7d53c703ed06b576a46c693dd77cd1022ddad87b
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-06-08 22:02:55 -0400

    Initial commit

---
+++ Controllers/HolaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ING_WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HolaController : ControllerBase
+    {
+        [HttpGet("saludo")]
+        public IActionResult Saludo([FromQuery] string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                return BadRequest(new
+                {
+                    mensaje = "Debe ingresar un nombre."
+                });
+            }
+
+            string mensajeBienvenida = "Bienvenido " + nombre;
+
+            return Ok(new
+            {
+                mensaje = mensajeBienvenida
+            });
+        }
+    }
+}
