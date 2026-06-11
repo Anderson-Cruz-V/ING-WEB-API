31ee0cad10a854a024d0ee3702407fd3bcb129dc
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-06-11 19:49:42 -0400

    NEW

--- Controllers/EdadController.cs
+++ Controllers/EdadController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ING_WEB_API.Controllers
 {
@@ -7,5 +6,78 @@ namespace ING_WEB_API.Controllers
     [ApiController]
     public class EdadController : ControllerBase
     {
+        [HttpGet("calcular")]
+        public IActionResult Calcular([FromQuery] DateTime fechaNacimiento)
+        {
+            DateTime fechaActual = DateTime.Today;
+
+            int edad = fechaActual.Year - fechaNacimiento.Year;
+
+            string signo = "";
+
+            if (fechaNacimiento.Date > fechaActual.AddYears(-edad))
+            {
+                edad = edad - 1;
+            }
+
+            int dia = fechaNacimiento.Day;
+            int mes = fechaNacimiento.Month;
+
+            if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19))
+            {
+                signo = "Aries";
+            }
+            else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20))
+            {
+                signo = "Tauro";
+            }
+            else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20))
+            {
+                signo = "Géminis";
+            }
+            else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 22))
+            {
+                signo = "Cáncer";
+            }
+            else if ((mes == 7 && dia >= 23) || (mes == 8 && dia <= 22))
+            {
+                signo = "Leo";
+            }
+            else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22))
+            {
+                signo = "Virgo";
+            }
+            else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22))
+            {
+                signo = "Libra";
+            }
+            else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21))
+            {
+                signo = "Escorpio";
+            }
+            else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21))
+            {
+                signo = "Sagitario";
+            }
+            else if ((mes == 12 && dia >= 22) || (mes == 1 && dia <= 19))
+            {
+                signo = "Capricornio";
+            }
+            else if ((mes == 1 && dia >= 20) || (mes == 2 && dia <= 18))
+            {
+                signo = "Acuario";
+            }
+            else
+            {
+                signo = "Piscis";
+            }
+
+            return Ok(new
+            {
+                fechaNacimiento = fechaNacimiento.ToShortDateString(),
+                edad = edad,
+                signoZodiacal = signo
+            });
+        }
     }
 }
---
+++ Controllers/NotasController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ING_WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotasController : ControllerBase
+    {
+        [HttpPost("estadisticas")]
+        public IActionResult Estadisticas([FromBody] DatosNotas datos)
+        {
+            double suma = 0;
+            double promedio = 0;
+            int notaMayor = 0;
+            int notaMenor = 0;
+            int aprobados = 0;
+            int reprobados = 0;
+
+            if (datos.notas.Count == 0)
+            {
+                return BadRequest(new
+                {
+                    mensaje = "Debe ingresar al menos una nota."
+                });
+            }
+
+            notaMayor = datos.notas[0];
+            notaMenor = datos.notas[0];
+
+            for (int i = 0; i < datos.notas.Count; i++)
+            {
+                int nota = datos.notas[i];
+
+                suma = suma + nota;
+
+                if (nota > notaMayor)
+                {
+                    notaMayor = nota;
+                }
+
+                if (nota < notaMenor)
+                {
+                    notaMenor = nota;
+                }
+
+                if (nota >= 70)
+                {
+                    aprobados = aprobados + 1;
+                }
+                else
+                {
+                    reprobados = reprobados + 1;
+                }
+            }
+
+            promedio = suma / datos.notas.Count;
+
+            return Ok(new
+            {
+                promedio = promedio,
+                notaMayor = notaMayor,
+                notaMenor = notaMenor,
+                aprobados = aprobados,
+                reprobados = reprobados
+            });
+        }
+    }
+
+    public class DatosNotas
+    {
+        public List<int> notas { get; set; } = new List<int>();
+    }
+}
