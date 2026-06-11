8e9c2d9632040aa3c951842157db40fb76196ffc
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-06-11 00:04:51 -0400

    TextoController

---
+++ Controllers/TextoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ING_WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextoController : ControllerBase
+    {
+        [HttpGet("contar")]
+        public IActionResult Contar([FromQuery] string texto)
+        {
+            int caracteres = 0;
+            int palabras = 1;
+            int vocales = 0;
+
+            if (texto == null || texto == "")
+            {
+                return BadRequest(new
+                {
+                    mensaje = "Debe ingresar un texto."
+                });
+            }
+
+            caracteres = texto.Length;
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                char letra = texto[i];
+
+                if (letra == ' ')
+                {
+                    palabras = palabras + 1;
+                }
+
+                if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+                    letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+                {
+                    vocales = vocales + 1;
+                }
+            }
+
+            return Ok(new
+            {
+                texto = texto,
+                palabras = palabras,
+                caracteres = caracteres,
+                vocales = vocales
+            });
+        }
+    }
+}
