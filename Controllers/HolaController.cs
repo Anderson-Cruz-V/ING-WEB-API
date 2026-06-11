ea5980a0e44ee92cbf7bdd09648e812d95568599
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-06-10 23:53:38 -0400

    commit

--- Controllers/HolaController.cs
+++ Controllers/HolaController.cs
@@ -1,67 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ING_WEB_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class NumerosController : ControllerBase
+    public class Controller : ControllerBase
     {
-        [HttpGet("analizar")]
-        public IActionResult Analizar([FromQuery] int n)
-        {
-            string paridad;
-            string tipoNumero;
-            bool esPrimo;
-
-            if (n % 2 == 0)
-            {
-                paridad = "Par";
-            }
-            else
-            {
-                paridad = "Impar";
-            }
-
-            if (n > 0)
-            {
-                tipoNumero = "Positivo";
-            }
-            else if (n < 0)
-            {
-                tipoNumero = "Negativo";
-            }
-            else
-            {
-                tipoNumero = "Cero";
-            }
-
-            esPrimo = VerificarPrimo(n);
-
-            return Ok(new
-            {
-                numero = n,
-                paridad = paridad,
-                primo = esPrimo ? "Es primo" : "No es primo",
-                tipo = tipoNumero
-            });
-        }
-
-        private bool VerificarPrimo(int numero)
-        {
-            if (numero <= 1)
-            {
-                return false;
-            }
-
-            for (int i = 2; i < numero; i++)
-            {
-                if (numero % i == 0)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
     }
 }
--- Controllers/NumerosController.cs
+++ Controllers/NumerosController.cs
@@ -1,28 +1,72 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ING_WEB_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class HolaController : ControllerBase
+    public class NumerosController : ControllerBase
     {
-        [HttpGet("saludo")]
-        public IActionResult Saludo([FromQuery] string nombre)
+        [HttpGet("analizar")]
+        public IActionResult Analizar([FromQuery] int n)
         {
-            if (string.IsNullOrWhiteSpace(nombre))
+            string paridad = "";
+            string tipo = "";
+            string primo = "";
+
+            if (n % 2 == 0)
             {
-                return BadRequest(new
-                {
-                    mensaje = "Debe ingresar un nombre."
-                });
+                paridad = "Par";
+            }
+            else
+            {
+                paridad = "Impar";
             }
 
-            string mensajeBienvenida = "Bienvenido " + nombre;
+            if (n > 0)
+            {
+                tipo = "Positivo";
+            }
+            else
+            {
+                if (n < 0)
+                {
+                    tipo = "Negativo";
+                }
+                else
+                {
+                    tipo = "Cero";
+                }
+            }
+
+            if (n == 2 || n == 3 || n == 5 || n == 7)
+            {
+                primo = "Es primo";
+            }
+            else
+            {
+                if (n <= 1)
+                {
+                    primo = "No es primo";
+                }
+                else
+                {
+                    if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+                    {
+                        primo = "No es primo";
+                    }
+                    else
+                    {
+                        primo = "Es primo";
+                    }
+                }
+            }
 
             return Ok(new
             {
-                mensaje = mensajeBienvenida
+                numero = n,
+                paridad = paridad,
+                tipo = tipo,
+                primo = primo
             });
         }
     }
