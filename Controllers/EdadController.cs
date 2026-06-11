069fd4da1c158666d0d381f76bfdf66af6d1d620
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-06-11 18:00:42 -0400

    Edad

---
+++ Controllers/EdadController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ING_WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EdadController : ControllerBase
+    {
+    }
+}
